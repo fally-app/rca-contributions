1fcf3f71858ecc3bc3b4a17566e84202c6df097b
Didier MUNEZERO <didiermunezer38@gmail.com>
2020-11-28 18:18:32 +0200

    adding axios and router

---
+++ client/src/router.js
@@ -0,0 +1,26 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      alias: "/members",
+      name: "members",
+      component: () => import("./components/MembersList"),
+    },
+    {
+      path: "/members/:id",
+      name: "member-details",
+      component: () => import("./components/Member"),
+    },
+    {
+      path: "members/add",
+      name: "add",
+      component: () => import("./components/AddMember"),
+    },
+  ],
+});
