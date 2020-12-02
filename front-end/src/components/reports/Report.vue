c261578584b17e639a98d238f216f4ef09c9363f
Didier MUNEZERO <didiermunezer38@gmail.com>
2020-12-02 11:42:56 +0200

    working overall report

--- front-end/src/components/reports/Report.vue
+++ front-end/src/components/reports/Report.vue
@@ -1,42 +1,35 @@
 <template>
-  <v-row align="center" class="list px-3 mx-auto">
-    <v-col cols="12" sm="12">
-      <v-card class="mx-auto" tile>
-        <v-card-title>
-          Overall Report
-          <v-spacer></v-spacer>
-        </v-card-title>
-        <v-form ref="form" lazy-validation>
-          <v-text-field
-            v-model="report.rows"
-            :rules="[(v) => !!v || 'First name is required']"
-            label="Rows"
-            required
-          ></v-text-field>
+  <div v-if="report" class="edit-form py-3">
+    <p class="headline">Overall Report</p>
 
-          <v-text-field
-            v-model="report.contributors"
-            :rules="[(v) => !!v || 'Last name is required']"
-            label="Contributors"
-            required
-          ></v-text-field>
+    <v-form ref="form" lazy-validation>
+      <v-text-field
+        v-model="report.rows"
+        label="Number of Contributions"
+        required
+      ></v-text-field>
 
-          <v-text-field
-            v-model="report.amount"
-            :rules="[(v) => !!v || 'Last name is required']"
-            label="Amount"
-            required
-          ></v-text-field>
+      <v-text-field
+        v-model="report.contributors"
+        label="Contributors"
+        required
+      ></v-text-field>
 
-          <v-divider class="my-5"></v-divider>
+      <v-text-field
+        v-model="report.amount"
+        label="Amount"
+        required
+      ></v-text-field>
 
-          <v-btn color="error" small class="mr-2" @click="deleteReport">
-            Delete
-          </v-btn>
-        </v-form>
-      </v-card>
-    </v-col>
-  </v-row>
+      <v-divider class="my-5"></v-divider>
+    </v-form>
+
+    <p class="mt-3">{{ message }}</p>
+  </div>
+
+  <div v-else>
+    <p>No report</p>
+  </div>
 </template>
 
 <script>
